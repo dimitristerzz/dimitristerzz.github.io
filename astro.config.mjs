339d24a08c455bfaad066867a9194482b943f421
Dimitris Terzidis <dterzidis2008@gmail.com>
2025-09-05 18:40:40 +0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://dimitristerzz.github.io/portfolio/",
-  base: "/portfolio/",
+  // site: "https://dimitristerzz.github.io/portfolio/",
+  // base: "/portfolio/",
 });
