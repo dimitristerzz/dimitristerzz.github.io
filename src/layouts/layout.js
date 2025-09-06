4b76fabe0239e8c0101281c4af2f0f1264f105fc
Dimitris Terzidis <dterzidis2008@gmail.com>
2025-09-06 18:43:51 +0300

    commit

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -1,39 +1,20 @@
-// import './globals.css'
+export const metadata = {
+  
+  icons: {
+    icon: '/assets/favicon.svg',
+  },
+  other: {
+    'http-equiv': 'X-UA-Compatible',
+    type: 'website',
+  },
+}
 
-// export const viewport = {
-//   width: 'device-width',
-//   initialScale: 1,
-//   maximumScale: 1,
-//   userScalable: 'no',
-// }
-
-// export const metadata = {
-//   title: 'Dimitris Terzidis - Portfolio',
-//   description: '',
-//   openGraph: {
-//     type: 'website',
-//     images: [
-//       {
-//         url: '',
-//       },
-//     ],
-//   },
-//   icons: {
-//     icon: '/assets/favicon.svg',
-//     apple: '/assets/favicon.svg',
-//   },
-//   other: {
-//     'http-equiv': 'X-UA-Compatible',
-//     type: 'website',
-//   },
-// }
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         {children}
-//       </body>
-//     </html>
-//   )
-// }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
